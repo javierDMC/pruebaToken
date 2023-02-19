a47a262ec0575dbc89d06c04675106152fafb0cf
javierDMC <javierdelmoralcamacho@gmail.com>
2023-02-19 12:39:34 +0100

    Sanctum token

--- database/factories/UsuarioFactory.php
+++ database/factories/UsuarioFactory.php
@@ -2,11 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Usuario;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
  */
 class UsuarioFactory extends Factory
 {
