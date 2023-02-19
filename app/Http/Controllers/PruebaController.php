a9960f04be4d26d4df140f701080b790fae46df6
javierDMC <javierdelmoralcamacho@gmail.com>
2023-02-19 10:44:47 +0100

    Previo a eleccion de token

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PruebaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return response()->json(['mensaje' => 'Accediendo a index']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): Response
+    {
+        return response()->json(['mensaje' => 'Insertando'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): Response
+    {
+        return response()->json(['mensaje' => 'Ficha de ' . $id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): Response
+    {
+        return response()->json(['mensaje' => 'Actualizando elemento']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): Response
+    {
+        return response()->json(['mensaje' => 'Borrando elemento']);
+    }
+}
---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UsuarioFactory extends Factory
+{
+
+    protected $model = Usuario::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'login' => $this->faker->word,
+            'password' => bcrypt('1234')
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return $this
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
